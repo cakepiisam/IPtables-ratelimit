d56b9b63898cdad2d5be1ad4b80028d8108d78b6
cakepiisam <90480524+cakepiisam@users.noreply.github.com>
2022-11-06 18:53:47 +0200

    Kirjotusvirheiden korjaus

--- ratelimit.sh
+++ ratelimit.sh
@@ -2,11 +2,11 @@ IPT=/sbin/iptables
 
 AIKA=120 # Kuinka ison ajan sisällä
 MAARA=60 # Monta kertaa kyseisen ajan sisällä
-INTERFACE="ens1" # interface
-PORT1= # portti lukema 1 esim http/https jne
-PORT2= # portti lukema 2 esim http/https jne
-PROTOCOL= # Protokolla esim TCP/UDP/ICMP jne
-APPEND=INPUT #
+INTERFACE="ens1" # Interface
+PORT1=http # Porttilukema 1 esim http/https jne
+PORT2=https # Porttilukema 2 esim http/https jne
+PROTOCOL=tcp # Protokolla esim TCP/UDP/ICMP jne
+APPEND=INPUT # INPUT/OUTPUT jne
 
 DACTION="reject" # Toiminto esim ACCPECT/DROP/REJECT jne
 $IPT -A ${APPEND} -p ${PROTOCOL} --dport ${PORT1} -i ${INTERFACE} -m state --state NEW -m recent --set
